b72ba63dc0b616767c2b25797eae73afa0ab32c5
Adrigondo <theadrigondo24@gmail.com>
2022-06-12 12:51:53 -0600

    Update: Refactor for rename changes

--- binding/src/app/app.module.ts
+++ binding/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { TestBindingComponent } from './test-binding/test-binding.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { ClassBindingComponent } from './class-binding/class-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    TestBindingComponent
+    InterpolationComponent,
+    PropertyBindingComponent,
+    ClassBindingComponent
   ],
   imports: [
     BrowserModule,
