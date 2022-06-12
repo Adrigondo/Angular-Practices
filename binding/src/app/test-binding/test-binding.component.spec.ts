43e7731b133660b173c9408182760ac15d0c6b4f
Adrigondo <theadrigondo24@gmail.com>
2022-06-12 00:33:27 -0600

    4_Add: Binding practice and reading input values

---
+++ binding/src/app/test-binding/test-binding.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestBindingComponent } from './test-binding.component';
+
+describe('TestBindingComponent', () => {
+  let component: TestBindingComponent;
+  let fixture: ComponentFixture<TestBindingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TestBindingComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TestBindingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
